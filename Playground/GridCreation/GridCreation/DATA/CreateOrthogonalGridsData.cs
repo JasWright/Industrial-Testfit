453d131d7eeb735931225dc03391b075bbafadf7
Jasmine Wright <jasmine.wright@dkgrar.onmicrosoft.com>
2019-01-18 10:33:22 -0500

    organized

--- Playground/GridCreation/GridCreation/DATA/CreateOrthogonalGridsData.cs
+++ Playground/GridCreation/GridCreation/DATA/CreateOrthogonalGridsData.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GridCreation.UTILS
+namespace GridCreation.DATA
 {
-    class Validation
+    class CreateOrthogonalGridsData
     {
     }
 }
